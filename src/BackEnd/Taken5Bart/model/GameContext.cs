fd0a47b1483b606618eb1f23d548946d442c2824
Jonas Koppen DRWork <dreaddrone@gmail.com>
2018-10-18 13:51:27 +0200

    DB toegevoegd

---
+++ src/BackEnd/Taken5Bart/model/GameContext.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace model
+{
+    class GameContext: DbContext
+    {
+        public GameContext(DbContextOptions<GameContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //  modelBuilder.Entity<Movie>().ToTable("Movie");
+            // modelBuilder.Entity<Rating>().ToTable("Rating");
+
+
+        }
+
+
+        public DbSet<Game> Games { get; set; }
+        public DbSet<Locatie> Locaties { get; set; }
+        public DbSet<Puzzel> Puzzels { get; set; }
+        public DbSet<Sessie> Sessies { get; set; }
+        public DbSet<Spelser> Spelers { get; set; }
+        public DbSet<Team> Teams { get; set; }
+    }
+}
