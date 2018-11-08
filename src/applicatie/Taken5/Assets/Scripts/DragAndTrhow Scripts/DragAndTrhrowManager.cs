a299ae673e48c7835c05d25d4fa2e6f7d0a9b737
segersviktor <segersviktor@hotmail.com>
2018-11-08 16:38:29 +0100

    brabogame basic code

---
+++ src/applicatie/Taken5/Assets/Scripts/DragAndTrhow Scripts/DragAndTrhrowManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndTrhrowManager : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other)
+    {
+        
+        if (other.gameObject.tag == "CatchObject")
+        {
+      
+            StartCoroutine("CatchObject", other.gameObject);
+
+        }
+    }
+        
+    IEnumerator CatchObject(GameObject CatchObject)
+    {
+        transform.Translate(Vector3.up, Space.World);
+        this.GetComponent<Rigidbody>().useGravity = false;
+        this.GetComponent<Rigidbody>().velocity = Vector3.zero;
+        Destroy(CatchObject.gameObject);
+        yield return new WaitForSeconds(1);
+        this.GetComponent<Rigidbody>().useGravity = true;
+        yield return new WaitForSeconds(1);
+        GameObject.FindGameObjectWithTag("Player").transform.LookAt(this.transform);
+        GameObject.FindGameObjectWithTag("Player").gameObject.GetComponent<Camera>().fieldOfView = 8.2f;
+
+    }
+
+}
+    
