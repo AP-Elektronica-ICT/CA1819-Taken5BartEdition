5f34d731eaccb2cc6e08c736b752b61a345e6140
User <user@LAPTOP-3.local>
2018-11-15 17:08:17 +0100

    making scene and startscript

---
+++ src/applicatie/Taken5/Assets/Scripts/APICall/PostRegistration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostRegistration : MonoBehaviour {
+
+    public Input Voornaam;
+    public Input Achternaam;
+	// Use this for initialization
+	void Start () 
+    {
+        string url = "";
+        WWWForm formDate = new WWWForm();
+        formDate.AddField("Voornaam", "Viktor");
+        formDate.AddField("Achternaam", "Segers");
+
+        WWW www = new WWW(url, formDate);
+		
+	}
+	
+    IEnumerator request (WWW www)
+    {
+        yield return www;
+
+    }
+}
