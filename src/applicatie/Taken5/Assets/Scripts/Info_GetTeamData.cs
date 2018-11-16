d05e7e6bd3d3644361b7ca7f7e7518b0f7c7ad03
Unknown <dreaddrone@gmail.com>
2018-11-16 14:08:55 +0100

    Opruim ongebruikte scripts + SOLID
    beter verspreiding van code zodat deze beter herbruikbaar is

---
+++ src/applicatie/Taken5/Assets/Scripts/Info_GetTeamData.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using SimpleJSON;
+
+/*
+ * Voor JSON pakketten die maar uit 1 laag bestaan is JsonUtility van unity zelf handig genoeg, maar voor json dat bestaat uit meerdere lagen is SimpleJSOn handiger
+ */ 
+
+public class GetTeamData : MonoBehaviour {
+
+    public string url;
+    // Use this for initialization
+    public Text SpelerNaam;
+    public Text TeamNaam;
+    public Text Diamanten;
+    public Text Score;
+    public Text TeamPositie;
+
+    private APICall api;
+
+    void Start()
+    {
+        if (Info.TeamId == -1)
+        {
+            Debug.Log("Dev team");
+            Info.Diamanten = 1;
+            Info.Score = 11111;
+            TeamPositie.text = "-1";
+        }
+        else
+        {
+            UpdateInfo();
+            ScorePos();
+        }
+        
+    }
+
+    void UpdateInfo()
+    {
+        Info.Update();
+        SpelerNaam.text = Info.SpelerNaam;
+        TeamNaam.text = Info.TeamNaam;
+        Diamanten.text = Info.Diamanten.ToString();
+        Score.text = Info.Score.ToString();
+        SpelerNaam.text = Info.Voornaam;
+    }
+    void ScorePos()
+    {
+        var url = "Team/" + Info.TeamId.ToString() + "/GetScorePosition";
+        Debug.Log(url);
+        var result = api.ApiCall(url);
+        TeamPositie.text = result;
+    }
+
+    [Serializable]
+    private class Speler
+    {
+        public double id { get; set; }
+        public string voornaam { get; set; }
+        public string achternaam { get; set; }
+    }
+    [Serializable]
+    private class Team
+    {
+        public double id { get; set; }
+        public string teamNaam { get; set; }
+        public List<Speler> spelers { get; set; }
+        public int diamantenVerzameld { get; set; }
+        public object puzzellijst { get; set; }
+        public int verzameldeDiamanten { get; set; }
+        public int score { get; set; }
+    }
+}
+
+
---
+++ src/applicatie/Taken5/Assets/Scripts/constante.cs
@@ -0,0 +1,29 @@
+﻿using SimpleJSON;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Info
+{
+    public static int spelerId = 2;
+    public static string Voornaam { get; set; }
+    public static int TeamId { get; set; }
+    public static string SpelerNaam { get; set; }
+    public static string TeamNaam { get; set; }
+    public static int Diamanten { get; set; }
+    public static int Score { get; set; }
+    public static void Update()
+    {
+        APICall api = new APICall();
+        string url = "Speler/" + Info.spelerId.ToString() + "/Team";
+        var N = JSON.Parse(api.ApiCall(url));
+        TeamNaam = N["teamNaam"].Value;
+        Diamanten = N["diamantenVerzameld"].AsInt;
+        Score = N["score"].AsInt;
+        TeamId = N["id"].AsInt;
+
+        url = "Speler/" + Info.spelerId.ToString();
+        N = JSON.Parse(api.ApiCall(url));
+        Voornaam = N["voornaam"].Value;
+    }
+}
