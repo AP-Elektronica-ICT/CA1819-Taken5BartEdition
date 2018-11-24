54478114ebf29a996931f780d9289ca2619b9b99
Unknown <segersviktor@hotmail.com>
2018-11-24 14:47:38 +0100

    Post in unity with hardcoded variables working

--- src/applicatie/Taken5/Assets/Scripts/APICall/PostRegistration.cs
+++ src/applicatie/Taken5/Assets/Scripts/APICall/PostRegistration.cs
@@ -1,30 +1,52 @@
-﻿using System.Collections;
+﻿using SimpleJSON;
+using System.Collections;
 using System.Collections.Generic;
+using System.Text;
 using UnityEngine;
+using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class postMessage : MonoBehaviour
+public class PostRegistration : MonoBehaviour
 {
-    // Use this for initialization
-    void Start()
-    {
-        string url = "http://localhost:1907/api/speler";
 
-        WWWForm formDate = new WWWForm();
-        formDate.AddField("voornaam", "sdfsd");
-        formDate.AddField("achternaam", "sdf");
-        formDate.AddField("deviceId", "15");
+    static string url = "http://localhost:1907/api/speler";
 
+    public void onclick()
+        {
+            Debug.Log("start");
+            JSONNode N = new JSONObject();
+           
 
-        WWW www = new WWW(url, formDate);
-
-        StartCoroutine(request(www));
-    }
+            N["voornaam"] = "sdfsdf";
+            N["achternaam"] = "sdfsdf";
+            N["deviceId"] = 269;
+            Debug.Log(N);
+            StartCoroutine(WWWPost(N));
+        }
 
     // Update is called once per frame
-    IEnumerator request(WWW www)
-    {
-        yield return www;
+    public static IEnumerator WWWPost(JSONNode N)
+        {
+            var req = new UnityWebRequest(url, "POST");
+            byte[] data = Encoding.UTF8.GetBytes(N.ToString());
 
+            req.uploadHandler = new UploadHandlerRaw(data);
+            req.uploadHandler.contentType = "application/json";
+            req.downloadHandler = new DownloadHandlerBuffer();
+
+            req.SetRequestHeader("Content-Type", "application/json");
+            req.SetRequestHeader("accept", "application/json");
+
+            yield return req.SendWebRequest();
+
+            if (req.isNetworkError || req.isHttpError)
+            {
+                Debug.Log(req.error);
+            }
+            else
+            {
+                Debug.Log("Form upload complete!");
+            }
+
+        }
     }
-}
